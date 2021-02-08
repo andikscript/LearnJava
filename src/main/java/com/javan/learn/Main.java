bc38de551a46556a1be51c2883aaceab57df193e
andikscript <andikscript@gmail.com>
2021-02-08 21:51:32 +0700

    update Main.java

--- src/main/java/com/javan/learn/Main.java
+++ src/main/java/com/javan/learn/Main.java
@@ -15,7 +15,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CountString countString = new CountString();
-        countString.countString();
+        
     }
 }
